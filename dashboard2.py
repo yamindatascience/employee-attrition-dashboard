74f92ed246e8ea2a32e3c417ef3416ce506b9519
Yamin Ghowrwal <99484965+yamindatascience@users.noreply.github.com>
2025-03-25 16:28:57 -0400

    Add files via upload

---
+++ dashboard2.py
@@ -0,0 +1,23 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "885522e6-0902-4f27-9eac-68bf6a770112",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "",
+   "name": ""
+  },
+  "language_info": {
+   "name": ""
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
